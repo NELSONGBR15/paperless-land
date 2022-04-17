6aee67630de8cc78caadec0b6f48c4e898e73bd4
NelsonFBR15 <bkanegrarivera@gmail.com>
2022-04-17 12:29:07 -0500

    Interfaz

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 export default class Admin extends Component{
     render(){
         return(
-            <div>Prueba 2</div>  
+            <div>Administrador</div>  
         )
     }
 }
