3656bdeeb00e66ba8257a26d1c313c253b1d9953
NelsonBR <nelson.bocanegra@campusvirtual.aunarvillavicencio.edu.co>
2022-04-16 21:02:16 -0500

    Prueba de interfaz 2

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,1 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
+
+export default class Admin extends Component{
+    render(){
+        return(
+            <div>Prueba 2</div>  
+        )
+    }
+}
